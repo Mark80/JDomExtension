6410d323527a6e9d578826722edad7b51224d321
Marco Tosini <mark.tosini@gmail.com>
2013-07-19 14:14:51 +0200

    Update JDomExtension.java

--- src/main/java/com/javaitaly/main/extension/JDomExtension.java
+++ src/main/java/com/javaitaly/main/extension/JDomExtension.java
@@ -20,12 +20,15 @@ import com.javaitaly.main.instanceInject.InjectXmlFileSystem;
 public class JDomExtension<X> implements Extension {
 
     private final Set<String> pathValue = new HashSet<String>();
-    private String path;
+
 
     public void addDocumentBean(@Observes AfterBeanDiscovery afterBeanDiscovery,final BeanManager beanManager) {
-        final InjectionTarget<Document> injectionTarget = beanManager.createInjectionTarget(beanManager
+       final InjectionTarget<Document> injectionTarget = beanManager.createInjectionTarget(beanManager
                 .createAnnotatedType(Document.class));
-        afterBeanDiscovery.addBean(new DocumentBean<Document>(injectionTarget, path, Document.class));
+        for (String path : pathValue) {
+            System.out.println(path);
+            afterBeanDiscovery.addBean(new DocumentBean<Document>(injectionTarget, path, Document.class));
+        }
     }
 
     public void processAnnotatedType(@Observes ProcessInjectionTarget<X> pit) {
@@ -44,7 +47,6 @@ public class JDomExtension<X> implements Extension {
             if (isXMLDocumnetAnnotation(ann)) {
                 final XMLDocument dodxml = (XMLDocument) ann;
                 pathValue.add(dodxml.path());
-                path = dodxml.path();
             }
         }
     }
